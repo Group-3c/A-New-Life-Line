8b8dde03b69d3fca5a5ca3e3e4c0a52b6c803cf8
carsonswope-uf <54452980+carsonswope-uf@users.noreply.github.com>
2019-12-03 09:34:56 -0500

    reverting router.post to router.route

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 let Events = require('../models/events');
 
-router.post('/new-event').post((req, res) => {
+router.route('/new-event').post((req, res) => {
     const name = req.body.name;
     const month = req.body.month;
     const date = req.body.date;
