a4a52f841cc6f1c781512a6c584dc75eeacfa9d6
Aimee Lewis <lewisaimeeg@gmail.com>
2019-12-02 20:44:14 -0500

    Added pages for Home menu submenus, edited footer so that it holds the links to social media

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../app.css';
 import './Footer.css';
+import facebookLogo from '../../assets/facebook.png';
+import linkedinLogo from '../../assets/linkedin.png';
 import Donate from "../../views/Donate/Donate"
 
 const Footer = () => {
@@ -15,9 +17,13 @@ const Footer = () => {
             </div>
             {/* Page Links */}
             <div className="footer-right">
-                {/*Section for link to Partner with Us*/}
-                {/*<Link className="footer-link" to="/Partner">Partner with Us</Link>*/}
-                Partner with Us
+
+                <div id="social-media">
+                  <a href="https://www.facebook.com/" target="_blank"><img src = {facebookLogo} alt="facebook logo"
+                   className="socialmedia-logo"></img></a>
+                  <a href="https://www.linkedin.com" target="_blank"><img src = {linkedinLogo} alt="linkedIn logo"
+                   className="socialmedia-logo"></img></a>
+                </div>
             </div>
 
 
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -18,34 +18,34 @@ function Home() {
                 <button class="dropbutton">Paradigm Shift</button>
                 <div class="dropdown-content">
                   <Link to="/AboutParadigmShift"><p>About</p></Link>
-                  <Link to="/"><p>Mentor Meetings</p></Link>
+                  <Link to="/MentorMeetings"><p>Mentor Meetings</p></Link>
                 </div>
               </div>
               <div class="dropdown">
                 <button class="dropbutton">Skill Building</button>
                 <div class="dropdown-content">
                   <Link to="/AboutSkillBuilding"><p>About</p></Link>
-                  <Link to="/"><p>Educational Opportunities</p></Link>
-                  <Link to="/"><p>Job Training</p></Link>
+                  <Link to="/EducationalOpportunities"><p>Educational Opportunities</p></Link>
+                  <Link to="/JobTraining"><p>Job Training</p></Link>
                 </div>
               </div>
               <div class="dropdown">
                 <button class="dropbutton">Entrepeneurship</button>
                 <div class="dropdown-content">
                   <Link to="/AboutEntrepeneurship"><p>About</p></Link>
-                  <Link to="/"><p>Goals and Planning</p></Link>
-                  <Link to="/"><p>Entrepeneurship Training</p></Link>
+                  <Link to="/GoalsPlanning"><p>Goals and Planning</p></Link>
+                  <Link to="/EntTraining"><p>Entrepeneurship Training</p></Link>
                 </div>
-                
+
               </div>
-              
+
             </div>
             <div className='col2'>
               <div className='dashCalendar'>
               <iframe src={calendarEmbed} height="350px" width="300px" frameborder="0" scrolling="no" id="calendarEmbed"/>
               </div>
             </div>
-            
+
           </div>
 
         </div>
