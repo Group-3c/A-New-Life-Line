906a5e6fb3f3bd4426f079a6bb366ee0cdf8710e
carsonswope-uf <54452980+carsonswope-uf@users.noreply.github.com>
2019-12-02 19:35:21 -0500

    Uploading events model

---
+++ server/models/events.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    month: {type: String, required: true, unique: false},
+    date: {type: String, required: true, unique: false},
+    type: {type: String, required: false, unique: false},
+    description: {type: String, required: true, unique: false},
+    address: {type: String, required: true, unique: false},
+});
+
+/* Use your schema to instantiate a Mongoose model */
+const Event = mongoose.model('Event', eventSchema);
+
+/* Export the model to make it avaiable to other parts of your Node application */
+module.exports = Event;
