4cffb4034f1a483b29872a0d2afd24b339138adc
Diego Coviella <covielladiego@ufl.edu>
2019-12-03 00:40:12 -0500

    cors

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
+  app.use(proxy("/*", { target: "https://a-new-life-line-server.herokuapp.com/" }));
 };
