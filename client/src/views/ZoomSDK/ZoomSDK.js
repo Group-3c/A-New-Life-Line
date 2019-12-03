f17622e5e7270cc9c5be9798186b262f358c4ce0
Derek Pearson <dpearson2699@gmail.com>
2019-12-02 22:12:22 -0500

    Attempted to fix the jquery error, but it's still partially broken

--- client/src/views/ZoomSDK/ZoomSDK.js
+++ client/src/views/ZoomSDK/ZoomSDK.js
@@ -1,77 +1,95 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ZoomMtg from 'zoomus-jssdk';
-import jquery from 'jQuery';
-import $ from 'jQuery';
+import jQuery from 'jquery';
+import $ from 'jquery';
+window.$ = window.jQuery = jQuery;
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/1.6.1/lib', '/av');
+(function(){
 
-console.log('checkSystemRequirements');
-console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+	console.log('checkSystemRequirements');
+	console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 
-ZoomMtg.preLoadWasm();
-ZoomMtg.prepareJssdk();
+    // it's option if you want to change the jssdk dependency link resources.
+    // ZoomMtg.setZoomJSLib('https://source.zoom.us/1.6.1/lib', '/av'); // CDN version default
+    // ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.6.1/lib', '/av'); // china cdn option 
+    // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/zoomus-jssdk/dist/lib', '/av'); // Local version default
+    ZoomMtg.preLoadWasm();
 
-const API_KEY = 'osh9e1ZkTueTBPGJK91gAg';
+    ZoomMtg.prepareJssdk();
+    
+    var API_KEY = 'osh9e1ZkTueTBPGJK91gAg';
 
-/**
- * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
- * The below generateSignature should be done server side as not to expose your api secret in public
- * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
- */
-const API_SECRET = 'SRi0FjpefpiSJVxntHjhii6oiGQB7lOpSZKM';
+    /**
+     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
+     * The below generateSignature should be done server side as not to expose your api secret in public
+     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
+     */
+    var API_SECRET = 'SRi0FjpefpiSJVxntHjhii6oiGQB7lOpSZKM';
 
-document.getElementById('join_meeting').addEventListener('click', (e) => {
-    e.preventDefault();
 
-    const meetConfig = {
-        apiKey: API_KEY,
-        apiSecret: API_SECRET,
-        meetingNumber: parseInt(document.getElementById('meeting_number').value, 10),
-        userName: document.getElementById('display_name').value,
-        passWord: '',
-        leaveUrl: 'https://zoom.us',
-        role: 0
-    };
+    document.getElementById('join_meeting').addEventListener('click', function(e){
+        e.preventDefault();
 
-    ZoomMtg.generateSignature({
-        meetingNumber: meetConfig.meetingNumber,
-        apiKey: meetConfig.apiKey,
-        apiSecret: meetConfig.apiSecret,
-        role: meetConfig.role,
-        success(res) {
-            console.log('signature', res.result);
-            ZoomMtg.init({
-                leaveUrl: 'http://www.zoom.us',
-                success() {
-                    ZoomMtg.join(
-                        {
-                            meetingNumber: meetConfig.meetingNumber,
-                            userName: meetConfig.userName,
-                            signature: res.result,
-                            apiKey: meetConfig.apiKey,
-                            userEmail: 'email@gmail.com',
-                            passWord: meetConfig.passWord,
-                            success() {
-                                $('#nav-tool').hide();
-                                console.log('join meeting success');
-                            },
-                            error(res) {
-                                console.log(res);
-                            }
-                        }
-                    );
-                },
-                error(res) {
-                    console.log(res);
-                }
-            });
+        if(!this.form.checkValidity()){
+            alert("Enter Name and Meeting Number");
+            return false;
         }
+
+        var meetConfig = {
+            apiKey: API_KEY,
+            apiSecret: API_SECRET,
+            meetingNumber: parseInt(document.getElementById('meeting_number').value),
+            userName: document.getElementById('display_name').value,
+            passWord: "",
+            leaveUrl: "https://zoom.us",
+            role: 0
+        };
+
+
+        var signature = ZoomMtg.generateSignature({
+            meetingNumber: meetConfig.meetingNumber,
+            apiKey: meetConfig.apiKey,
+            apiSecret: meetConfig.apiSecret,
+            role: meetConfig.role,
+            success: function(res){
+                console.log(res.result);
+            }
+        });
+
+        ZoomMtg.init({
+            leaveUrl: 'http://www.zoom.us',
+            isSupportAV: true,
+            success: function () {
+                ZoomMtg.join(
+                    {
+                        meetingNumber: meetConfig.meetingNumber,
+                        userName: meetConfig.userName,
+                        signature: signature,
+                        apiKey: meetConfig.apiKey,
+                        userEmail: 'email@gmail.com',
+                        passWord: meetConfig.passWord,
+                        success: function(res){
+                            $('#nav-tool').hide();
+                            console.log('join meeting success');
+                        },
+                        error: function(res) {
+                            console.log(res);
+                        }
+                    }
+                );
+            },
+            error: function(res) {
+                console.log(res);
+            }
+        });
+
     });
-});
+
+})();
 
 function ZoomSDK(){
     return(
-      <iframe title={'Zoom_HTML'} src={"../ZoomSDK.html"}/>
+      <iframe title={'Zoom_HTML'} src={"../../../public/ZoomSDK.html"}/>
     );
 }
 
